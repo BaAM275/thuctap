992dce3763966dfc9f4bd88e425e5fb442cd2dbd
BaAM275 <79955012+BaAM275@users.noreply.github.com>
2022-07-06 10:06:04 +0700

    đọc file, ghi filestream

---
+++ test2/test2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string filename = "abc.txt";
+            string content = File.ReadAllText(filename);
+            Console.WriteLine(content);   
+         }
+    }
+}
