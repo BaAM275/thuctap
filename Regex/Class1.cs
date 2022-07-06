7bbc61002b49fff480b089af0b74a7a629517968
BaAM275 <79955012+BaAM275@users.noreply.github.com>
2022-07-06 13:21:22 +0700

    extension method string

---
+++ Regex/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexVali
+{
+    public static class exTion
+    {
+        //from 9 to 15 numbers
+        public static void Sdt(this String a)
+        {
+            var r = new Regex(@"^[0-9\-\+]{9,15}$");
+            if (!r.IsMatch(a))
+            {
+                Console.WriteLine($"{a} sdt doesn't match");
+            }
+            else
+            {
+                Console.WriteLine($"{a} sdt matches");
+            }
+        }
+        //Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+        public static void Pass(this String a)
+        {
+            var r = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
+            if (!r.IsMatch(a))
+            {
+                Console.WriteLine($"{a} password doesn't match");
+            }
+            else
+            {
+                Console.WriteLine($"{a} password  match");
+            }
+        }
+        public static void email(this String a)
+        {
+            var r = new Regex(@"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$");
+            if (!r.IsMatch(a))
+            {
+                Console.WriteLine($"{a} email doesn't match");
+            }
+            else
+            {
+                Console.WriteLine($"{a} email match");
+            }
+        }
+    }
+}
---
+++ Regex/Program.cs
@@ -0,0 +1,24 @@
+﻿using RegexVali;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Regexvali
+{
+    class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            var cc1 = "089754466";
+            var test = "mysite@ourearth.com";
+            test.email();
+            cc1.Sdt();
+
+            string vali = null;
+            vali = Console.ReadLine();
+            vali.Pass();
+            vali.email();
+            vali.Sdt();
+        }
+    }
+}
