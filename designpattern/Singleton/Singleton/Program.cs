bfca66060d96e076caa121c085ccc5e7913b493b
BaAM275 <79955012+BaAM275@users.noreply.github.com>
2022-07-07 09:36:52 +0700

    Add files via upload

---
+++ designpattern/Singleton/Singleton/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Singleton
+{
+
+
+    class MainApp
+
+    {
+    
+        static void Main()
+        {
+       
+
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            // Test for same instance
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+
+     
+
+            Console.ReadKey();
+        }
+    }
+
+  
+    class Singleton
+
+    {
+        private static Singleton _instance;
+
+ 
+
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            
+
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
